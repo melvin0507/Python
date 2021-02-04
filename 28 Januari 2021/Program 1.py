6dcc89f38292dc13ed0d06b9080fb708f79cafd0
melvin0507 <78136167+melvin0507@users.noreply.github.com>
2021-02-04 07:44:01 +0700

    Delete 28 Januari 2021 directory

--- 28 Januari 2021/Program 1.py
+++
@@ -1,9 +0,0 @@
-def konversi(desimal):
-    if desimal > 1:
-        konversi (desimal//2)
-    print(desimal % 2, end=' ')
-
-desimal = int(input('input nilai desimal : '))
-
-konversi(desimal)
-print()
